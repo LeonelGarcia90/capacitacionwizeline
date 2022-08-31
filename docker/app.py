671d9d6803c99ed286558f402e119d7b8886aa34
LeonelGarcia90 <leonelgarciaroldan90@gmail.com>
2022-08-31 15:09:52 -0500

    Cambios en archivos

--- docker/app.py
+++ docker/app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! FROM DOCKER THANKS! I have been seen {} times.\n'.format(count)
+    return 'Hola desde docker, me han visitado: {} veces\n'.format(count)
